3f91ef3c4fa294d1be246fc0748eb1d9c2a9c8e8
JoshuaParungao <jmphotography1830@gmail.com>
2025-10-29 08:19:19 +0800

    CSRF Update

--- MyBackEnd/settings.py
+++ MyBackEnd/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+ "https://peitel1-mybackend-1.onrender.com",
+ ]
