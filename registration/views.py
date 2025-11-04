be271e295fbae870b1194906d75f385f643625de
JoshuaParungao <jmphotography1830@gmail.com>
2025-11-04 09:27:19 +0800

    Update and Delete function Added

--- registration/views.py
+++ registration/views.py
@@ -19,8 +19,32 @@ def register_user(request):
 
 @api_view(['GET'])
 def list_user(request):
-  # Retrieve all users and return serialized data
+
   users = UserRegistration.objects.all()
   serializer = UserRegistrationSerializer(users, many=True)
   return Response(serializer.data, status=status.HTTP_200_OK)
-        
+
+
+@api_view(['GET','PUT', 'DELETE'])
+def user_detail(request, pk):
+  try:
+    user = UserRegistration.objects.get(pk=pk)
+  except UserRegistration.DoesNotExist:
+    return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+  if request.method == 'GET':
+    serializer = UserRegistrationSerializer(user)
+    return Response(serializer.data)
+
+  elif request.method == 'PUT':
+    serializer = UserRegistrationSerializer(user, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    else:
+      print("serializer errors:",serializer.errors)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  elif request.method == 'DELETE':
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
